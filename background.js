757d5bcdbd2d94d9a8dff871831e9fbf29f52540
Leo Huynh <huynhtuananh21895@gmail.com>
2021-12-13 23:58:51 +0700

    Update bg

--- background.js
+++ background.js
@@ -1,9 +1,8 @@
 let color = '#3aa757';
 
-// chrome.runtime.onInstalled.addListener(() => {
-//   chrome.storage.sync.set({ color });
-//   console.log('Default background color set to %cgreen', `color: ${color}`);
-// });
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("=========> - onInstalled")
+});
 console.log("=========> - test")
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
