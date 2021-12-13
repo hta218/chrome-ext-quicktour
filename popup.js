6795c06cec0f0c8a75700c725b0378ebd3367f87
Leo Huynh <huynhtuananh21895@gmail.com>
2021-12-13 23:58:58 +0700

    Add popup checklist

--- popup.js
+++ popup.js
@@ -1,6 +1,20 @@
 const THEME_CHECK_API = "https://foxkit.app/api/public/extension/theme"
 const EXT_KEY = 'FOX_X'
 
+console.log('Run popup.js')
+
+// 1. Detect Shopify site
+
+
+// 2. Detect Theme
+
+
+// 2.1 Is Minimog => Fetch theme version
+
+// 2.2 Not Minimog ? Fetch theme list (Check Shop + get Theme list)
+
+// 3. Add event to fetch all themes
+
 // Initialize button with user's preferred color
 const getPageTitleButton = document.getElementById("getTitle");
 const titleNode = document.getElementById("title");
@@ -8,7 +22,6 @@ const resNode = document.getElementById("res");
 
 getPageTitleButton.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  await chrome.storage.sync.clear()
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     function: getPageTitle
@@ -17,11 +30,9 @@ getPageTitleButton.addEventListener("click", async () => {
 
 
 async function getPageTitle() {
-  const res = await (await fetch('${THEME_CHECK_API}?shop=hta218.myshopify.com&themeId=125944889514')).json()
+  const res = await (await fetch(`${THEME_CHECK_API}?shop=hta218.myshopify.com&themeId=125944889514`)).json()
   chrome.runtime.sendMessage({
     type: EXT_KEY,
     theme: res.payload
   });
-
-  return res
 }
