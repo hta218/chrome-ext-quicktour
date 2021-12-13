873f490811c791f41cd77928b89855f81ee6361f
Leo Huynh <huynhtuananh21895@gmail.com>
2021-12-13 23:23:30 +0700

    Update popup

--- popup.js
+++ popup.js
@@ -1,10 +1,10 @@
+const THEME_CHECK_API = "https://foxkit.app/api/public/extension/theme"
+const EXT_KEY = 'FOX_X'
+
 // Initialize button with user's preferred color
 const getPageTitleButton = document.getElementById("getTitle");
 const titleNode = document.getElementById("title");
 const resNode = document.getElementById("res");
-console.log("=========> - titleNode", titleNode)
-
-var port = chrome.runtime.connect();
 
 getPageTitleButton.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -12,29 +12,14 @@ getPageTitleButton.addEventListener("click", async () => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     function: getPageTitle
-  }, async () => {
-    chrome.storage.sync.get("theme", ({ theme }) => {
-      console.log("=========> - theme", theme)
-    });
   });
 })
 
 
 async function getPageTitle() {
-  console.log("=========> - document.title", document.title)
-  // chrome.storage.sync.set({ title: document.title });
-  // chrome.storage.sync.set({ foo: { bar: 1, baz: [2, 3, 4] } });
-  const res = await (await fetch('https://foxkit.app/api/public/extension/theme?shop=hta218.myshopify.com&themeId=125944889514')).json()
-  console.log("=========> - res", res)
-  await chrome.storage.sync.set({ theme: res.payload });
-
-  chrome.runtime.sendMessage('get-user-data', (response) => {
-    // 3. Got an asynchronous response with the data from the background
-    console.log('received user data', response);
-  });
-
+  const res = await (await fetch('${THEME_CHECK_API}?shop=hta218.myshopify.com&themeId=125944889514')).json()
   chrome.runtime.sendMessage({
-    type: 'FOX_X',
+    type: EXT_KEY,
     theme: res.payload
   });
 
