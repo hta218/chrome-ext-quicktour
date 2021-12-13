a3df6f9c31174a81f23594cb33c80d680ee7c9b7
Leo Huynh <huynhtuananh21895@gmail.com>
2021-12-13 23:23:26 +0700

    Update backgrond script

--- background.js
+++ background.js
@@ -1,17 +1,10 @@
 let color = '#3aa757';
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log("=========> - message", message)
-  // 2. A page requested user data, respond with a copy of `user`
-  if (message === 'get-user-data') {
-    sendResponse("user");
-  }
-});
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.sync.set({ color });
+//   console.log('Default background color set to %cgreen', `color: ${color}`);
+// });
+console.log("=========> - test")
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log("=========> - request", request)
